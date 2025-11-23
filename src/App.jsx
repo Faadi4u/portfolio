9fa643366c4aae3ac63badc3c44b47d038b67237
Fahad <road24u@gmail.com>
2025-11-23 09:52:13 +0500

    Initial commit: React + Tailwind + GSAP setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./App.css"
+// import gsap from 'gsap';
+// import { useGSAP } from '@gsap/react';
+
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
